04bcbe11af3e6e4e16a090cfb13f440c75b86745
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-06-07 10:49:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def send_audio(update, context, audio_file_path):
         # Отправляем аудиофайл пользователю
         context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(audio_file_path, 'rb'))
     except Exception as e:
-        print("Произошла ошибка при отправке аудио:", e)
+        print("Ses gönderilirken bir hata oluştu:", e)
     finally:
         # Удаляем временный файл после отправки
         os.remove(audio_file_path)
@@ -28,7 +28,7 @@ def download_and_convert_audio(update, context):
         # Получаем ссылку на YouTube видео из сообщения пользователя
         video_url = update.message.text.split(' ')[0]
 
-        print("Ссылка на видео:", video_url)  # Добавляем логирование
+        print("Video bağlantısı:", video_url)  # Добавляем логирование
 
         # Создаем временный каталог для сохранения аудиофайла
         with tempfile.TemporaryDirectory() as tmp_dir:
@@ -51,19 +51,19 @@ def download_and_convert_audio(update, context):
             # Отправляем аудиофайл пользователю
             context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(f'{tmp_dir}/audio.mp3', 'rb'))
     except IndexError as e:
-        print("Произошла ошибка IndexError:", e)
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Не удалось найти аудио в этом видео.")
+        print("bir hata oluştu IndexError:", e)
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Bu video ile ses bulunamadı.")
     except Exception as e:
-        print("Произошла ошибка при загрузке видео и извлечении аудио:", e)
+        print("Video yüklenirken veya ses çıkartılırken bir hata oluştu:", e)
 
 
 # Функция для обработки команды /start
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Привет! Отправь мне ссылку на YouTube видео, чтобы я отправил тебе аудио в формате mp3.')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Merhaba! Bana YouTube videosunun bağlantısını gönderin, böylece size sesi mp3 formatında gönderebileyim.')
 
 def main():
     # Инициализация бота с вашим токеном
-    updater = Updater("6495248480:AAHO9TmMY0-wbrQntYwqiroQDEXU0_8ClaQ", use_context=True)
+    updater = Updater("7218232167:AAHiNANcjt6PggG-92Hijy2Jx5GMUW7BCqo", use_context=True)
 
     # Получаем диспетчер для регистрации обработчиков
     dp = updater.dispatcher
